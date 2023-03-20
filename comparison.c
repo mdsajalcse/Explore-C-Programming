57326a2b7c1663fa209c92e0fbf744ed6b4e7023
ifthekar <sajalcse2018@gmail.com>
2023-03-20 17:07:56 +0600

    comparison operator using process

---
+++ comparison.c
@@ -0,0 +1,23 @@
+
+// comparison operator in c
+ #include<stdio.h>
+ int main(){
+ int x,y,z;
+ x=10;y=10;z=89;
+ // ==	Equal to	x == y
+  printf("Equal To Operator in C::%d\n",x==y);
+  // !=	Not equal	x != y
+  printf("Not Equal Operator in C::%d\n", x!=z);
+  // >	Greater than	x > y
+  printf("Greater than Operator in C::%d\n",x>y);
+  // <	Less than	x < y
+   printf("Less than Operator in C::%d\n",x<y);
+   // >=	Greater than or equal to	x >= y
+   printf("Greater than or Equal Operator in C::%d\n",x>=y);
+   // <=	Less than or equal to	x <= y
+   printf("Less than or Equal Operator in C::%d\n",x<=y);
+ return 0;
+ }
+
+//logical operator in c
+//bitwise operator in c
