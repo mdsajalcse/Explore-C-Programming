77bb4c1327380c5e3bf14501c0989c9d12ca794e
ifthekar <sajalcse2018@gmail.com>
2023-03-20 09:59:29 +0600

    how to use various data types in c

---
+++ datatype.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(){
+
+
+//Basic Data Types
+
+int a=20; //
+float b=30.32;
+double c=40.4444;
+char ch='p';
+//Format Specifier
+printf(" integer data type value::%d\n",a);
+printf("Float data type number is::%.3f\n",b);
+printf("double data type value is::%.3lf\n",c);
+printf("Character data type Value is::%c",ch);
+
+
+return 0;
+}
