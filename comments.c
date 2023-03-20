802952afe01ab4aa3f9586c446b8aaf6a25bb90b
ifthekar <sajalcse2018@gmail.com>
2023-03-20 09:09:09 +0600

    c programming comments added

---
+++ comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+int a,b,c;  //this is integer data type
+float x,y,z; //this is float data type
+double area ; // this is double data type
+long long int value; // this is long long data type
+int marks[30]; //this is array data type
+//another data type is string , pointer , character, structure, void
+
+
+return 0;
+}
