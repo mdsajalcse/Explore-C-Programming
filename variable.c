703ab0172d045dfdbeaa9db4fb2d662e3be81d9a
ifthekar <sajalcse2018@gmail.com>
2023-03-20 09:49:22 +0600

    added how to use variable in c programming

---
+++ variable.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+  //create variables
+  int myIntNum=19;
+  float myFloatNum=10.33;
+  double myDoubleNum=10.888888;
+  char myCharLetter='A';
+
+
+
+ //size of data type
+  printf("Size of int is::%d\n", sizeof(myIntNum));
+  printf("Size of Float is:: %d\n", sizeof(myFloatNum));
+  printf("size of Double is:: %d\n",sizeof(myDoubleNum));
+  printf("size of character is::%d\n",sizeof(myCharLetter));
+
+return 0;
+}
---
+++ variableprint.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+
+//variable printing
+
+int a=20; //assign variable
+float b=30.32;
+double c=40.4444;
+char ch='p';
+printf("integer value is::%d\n",a);
+printf("Float number is::%f\n",b);
+printf("double value is::%lf\n",c);
+printf("Character Value is::%c",ch);
+
+
+return 0;
+}
